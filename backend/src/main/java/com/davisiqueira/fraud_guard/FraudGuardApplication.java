591b92961533bd4f7d99da385a973bb2fa7ae48a
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-17 15:32:52 -0300

    chore: first commit

---
+++ backend/src/main/java/com/davisiqueira/fraud_guard/FraudGuardApplication.java
@@ -0,0 +1,13 @@
+package com.davisiqueira.fraud_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudGuardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/davisiqueira/fraud_guard/FraudGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davisiqueira.fraud_guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
