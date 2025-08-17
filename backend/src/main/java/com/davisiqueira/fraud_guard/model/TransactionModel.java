8824cf6a733957f5d806c1fd8c96f9f4e33e4973
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-17 17:14:07 -0300

    feat(backend): add transaction model

---
+++ backend/src/main/java/com/davisiqueira/fraud_guard/model/TransactionModel.java
@@ -0,0 +1,36 @@
+package com.davisiqueira.fraud_guard.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "transactions")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class TransactionModel {
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    private Instant date;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private Float lat;
+
+    @Column(nullable = false)
+    private Float lon;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+    private Boolean suspect = false;
+}
