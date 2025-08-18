ff316bc0687064af0b55dadf0af38eba488b80ce
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-18 18:44:59 -0300

    feat(backend): implement logic to decide if a transaction is suspect or not

--- backend/src/main/java/com/davisiqueira/fraud_guard/repository/TransactionRepository.java
+++ backend/src/main/java/com/davisiqueira/fraud_guard/repository/TransactionRepository.java
@@ -2,11 +2,20 @@ package com.davisiqueira.fraud_guard.repository;
 
 import com.davisiqueira.fraud_guard.model.TransactionModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionModel, Long> {
     List<TransactionModel> findAllByCpf(String cpf);
+
+    @Query(value = "SELECT * FROM transactions t WHERE t.suspect = false ORDER BY RANDOM() LIMIT :limit;", nativeQuery = true)
+    List<TransactionModel> getRandomSample(@Param("limit") int limit);
+
+    @Query(value = "SELECT COUNT(*) FROM transactions t WHERE t.date >= :date", nativeQuery = true)
+    int countTransactionSince(@Param("date") LocalDateTime date);
 }
--- backend/src/main/java/com/davisiqueira/fraud_guard/service/fraud/FraudDetectionService.java
+++ backend/src/main/java/com/davisiqueira/fraud_guard/service/fraud/FraudDetectionService.java
@@ -2,19 +2,89 @@ package com.davisiqueira.fraud_guard.service.fraud;
 
 import com.davisiqueira.fraud_guard.model.TransactionModel;
 import com.davisiqueira.fraud_guard.repository.TransactionRepository;
+import com.davisiqueira.fraud_guard.util.FraudDetectionUtils;
+import com.davisiqueira.fraud_guard.util.geolocalization.Coordinates;
+import com.davisiqueira.fraud_guard.util.geolocalization.GeolocalizationUtils;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class FraudDetectionService {
-    private TransactionRepository repository;
+    private final TransactionRepository repository;
+    private static final int SAMPLE_SIZE = 10;
+    private static final double ALLOWED_RADIUS = 1000;
+    private static final int FREQUENCY_TIME_WINDOW_MINUTES = 5;
+    private static final int MAX_TRANSACTIONS_ALLOWED_IN_WINDOW = 5;
+
 
     public FraudDetectionService(TransactionRepository repository) {
         this.repository = repository;
     }
 
     public boolean isSuspect(TransactionModel transaction) {
-        return transaction.getValue().compareTo(BigDecimal.valueOf(10000)) > 0;
+        int score = 0;
+
+        List<TransactionModel> transactions = repository.getRandomSample(SAMPLE_SIZE);
+
+        if (isHighFrequency()) {
+            score += 10;
+        }
+
+        if (isOutlierValue(transactions, transaction)) {
+            score += 50;
+        }
+
+        if (isOutOfAllowedRadius(transactions, transaction)) {
+            score += 40;
+        }
+
+        return score >= 50;
+    }
+
+    private boolean isHighFrequency() {
+        int countLastFiveMinutes = repository.countTransactionSince(LocalDateTime.now().minusMinutes(FREQUENCY_TIME_WINDOW_MINUTES));
+
+        return countLastFiveMinutes >= MAX_TRANSACTIONS_ALLOWED_IN_WINDOW;
+    }
+
+    private boolean isOutlierValue(List<TransactionModel> transactions, TransactionModel transaction) {
+        List<BigDecimal> values = transactions
+                .stream()
+                .map(TransactionModel::getValue)
+                .toList();
+
+        BigDecimal average = values
+                .stream()
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .divide(BigDecimal.valueOf(values.size()), RoundingMode.HALF_UP);
+
+        BigDecimal standardDeviation = FraudDetectionUtils.calculateStandardDeviation(values);
+
+        return isSuspectValue(transaction.getValue(), average, standardDeviation);
+    }
+
+    private boolean isSuspectValue(BigDecimal value, BigDecimal average, BigDecimal standardDeviation) {
+        BigDecimal thresholdMultiplier = BigDecimal.valueOf(3);
+
+        BigDecimal threshold = average.add(standardDeviation.multiply(thresholdMultiplier));
+
+        int comparison = value.compareTo(threshold);
+
+        return comparison >= 0;
+    }
+
+    private boolean isOutOfAllowedRadius(List<TransactionModel> transactions, TransactionModel transaction) {
+        List<Coordinates<Double>> coordinates = transactions
+                .stream()
+                .map(t -> new Coordinates<>(t.getLat(), t.getLon()))
+                .toList();
+
+        Coordinates<Double> point = new Coordinates<>(transaction.getLat(), transaction.getLon());
+
+        return !GeolocalizationUtils.isPointNearGroup(point, coordinates, ALLOWED_RADIUS);
     }
 }
