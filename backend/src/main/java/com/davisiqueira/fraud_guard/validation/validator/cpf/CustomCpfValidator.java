18e64f739f75afa9d4326d5b762740cec340fd75
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-19 22:37:31 -0300

    feat(backend): add ValidCpf custom annotation and validator

---
+++ backend/src/main/java/com/davisiqueira/fraud_guard/validation/validator/cpf/CustomCpfValidator.java
@@ -0,0 +1,40 @@
+package com.davisiqueira.fraud_guard.validation.validator.cpf;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CustomCpfValidator implements ConstraintValidator<ValidCpf, String> {
+
+    @Override
+    public boolean isValid(String cpf, ConstraintValidatorContext constraintValidatorContext) {
+        if(cpf == null || !cpf.matches("\\d{11}")) {
+            return false;
+        }
+
+        return isValidCpf(cpf);
+    }
+
+    private boolean isValidCpf(String cpf) {
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        try {
+            int d1 = 0, d2 = 0;
+
+            for (int i = 0; i < 9; i++) {
+                int digit = cpf.charAt(i) - '0';
+                d1 += digit * (10 - i);
+                d2 += digit * (11 - i);
+            }
+
+            d1 = d1 % 11 < 2 ? 0 : 11 - (d1 % 11);
+            d2 += d1 * 2;
+            d2 = d2 % 11 < 2 ? 0 : 11 - (d2 % 11);
+
+            return d1 == (cpf.charAt(9) - '0') && d2 == (cpf.charAt(10) - '0');
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ backend/src/main/java/com/davisiqueira/fraud_guard/validation/validator/cpf/ValidCpf.java
@@ -0,0 +1,16 @@
+package com.davisiqueira.fraud_guard.validation.validator.cpf;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CustomCpfValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidCpf {
+    String message() default "Invalid CPF";
+    Class<?>[] groups() default {};
+    Class<? super Payload>[] payload() default {};
+}
