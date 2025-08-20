8b649c486dc3b365352566b8f07bb36140b13400
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-20 16:35:18 -0300

    refactor(backend): add errors map to better handle exceptions

--- backend/src/main/java/com/davisiqueira/fraud_guard/config/GlobalExceptionHandler.java
+++ backend/src/main/java/com/davisiqueira/fraud_guard/config/GlobalExceptionHandler.java
@@ -2,31 +2,68 @@ package com.davisiqueira.fraud_guard.config;
 
 import com.davisiqueira.fraud_guard.exception.ApiErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
-    public ResponseEntity<ApiErrorResponse> handleValidationErrors(Exception e, HttpServletRequest httpServletRequest) {
-        ApiErrorResponse response = buildApiErrorResponse(e, httpServletRequest);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationError(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
 
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        Map<String, String> errors = e.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        err -> Optional.ofNullable(err.getDefaultMessage()).orElse("Invalid value"),
+                        (err1, err2) -> err1
+                ));
+
+        ApiErrorResponse response = buildResponse("Validation error", request, status, errors);
+
+        return new ResponseEntity<>(response, status);
     }
 
-    private ApiErrorResponse buildApiErrorResponse(Exception e, HttpServletRequest httpServletRequest) {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiErrorResponse> handleConstraintError(ConstraintViolationException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        Map<String, String> errors = e.getConstraintViolations()
+                .stream()
+                .collect(Collectors.toMap(
+                        v -> extractPath(v.getPropertyPath().toString()),
+                        ConstraintViolation::getMessage,
+                        (err1, err2) -> err1
+                ));
+
+        ApiErrorResponse response = buildResponse("Validation error", request, status, errors);
+
+        return new ResponseEntity<>(response, status);
+    }
+
+    private ApiErrorResponse buildResponse(String message, HttpServletRequest request, HttpStatus status, Map<String, String> errors) {
         return new ApiErrorResponse(
-                e.getMessage(),
-                httpServletRequest.getRequestURI(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                message,
+                request.getRequestURI(),
+                status.value(),
+                errors,
                 LocalDateTime.now()
         );
+    }
+
+    private String extractPath(String path) {
+        String[] parts = path.split("\\.");
+        return parts[parts.length - 1];
     }
 }
--- backend/src/main/java/com/davisiqueira/fraud_guard/exception/ApiErrorResponse.java
+++ backend/src/main/java/com/davisiqueira/fraud_guard/exception/ApiErrorResponse.java
@@ -1,12 +1,13 @@
 package com.davisiqueira.fraud_guard.exception;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public record ApiErrorResponse (
         String message,
         String path,
         int statusCode,
-        String error,
+        Map<String, String> errors,
         LocalDateTime timestamp
 ) {
 }
