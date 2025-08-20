6d2d96b70badaa92d2625f267461512a3b74cea0
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-20 20:16:26 -0300

    feat(backend): add jwt service

---
+++ backend/src/main/java/com/davisiqueira/fraud_guard/service/user/JwtService.java
@@ -0,0 +1,54 @@
+package com.davisiqueira.fraud_guard.service.user;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${spring.security.secret}")
+    private static String SECRET;
+
+    @Value("${spring.security.issuer}")
+    private static String ISSUER;
+
+    private static final Algorithm algorithm = Algorithm.HMAC256(SECRET);
+
+    public String generateToken(UserDetailsImpl user) throws JWTCreationException {
+            return JWT.create()
+                    .withIssuer(ISSUER)
+                    .withIssuedAt(issuedAt())
+                    .withExpiresAt(expiresAt())
+                    .withSubject(user.getUsername())
+                    .sign(algorithm);
+    }
+
+    public String getSubjectFromToken(String token) throws JWTVerificationException {
+        return JWT.require(algorithm)
+                .withIssuer(ISSUER)
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+    private Date issuedAt() {
+        return toDate(LocalDateTime.now());
+    }
+
+    private Date expiresAt() {
+        return toDate(LocalDateTime.now().plusHours(12));
+    }
+
+    private Date toDate(LocalDateTime dateTime) {
+        return Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+
+}
