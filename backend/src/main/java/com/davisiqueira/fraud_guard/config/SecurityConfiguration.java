a802826672c5e5a2b78bed289fa88c8074302bda
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-21 16:46:53 -0300

    feat(backend): implement the security filter chain

--- backend/src/main/java/com/davisiqueira/fraud_guard/config/SecurityConfiguration.java
+++ backend/src/main/java/com/davisiqueira/fraud_guard/config/SecurityConfiguration.java
@@ -1,14 +1,60 @@
 package com.davisiqueira.fraud_guard.config;
 
+import com.davisiqueira.fraud_guard.security.UserAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final UserAuthenticationFilter userAuthenticationFilter;
+
+    public SecurityConfiguration(UserAuthenticationFilter userAuthenticationFilter) {
+        this.userAuthenticationFilter = userAuthenticationFilter;
+    }
 
     public static final String[] ENDPOINTS_WITH_NO_AUTHENTICATION = {
             "/users/login",
             "/users"
     };
+
+    public static final String[] ENDPOINTS_ADMIN = {
+            "/users"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(ENDPOINTS_WITH_NO_AUTHENTICATION).permitAll()
+                        .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                // Registers `UserAuthenticationFilter` to run before `UsernamePasswordAuthenticationFilter` in the filter chain.
+                .addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                // It's possible to add multiple filters here.
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
