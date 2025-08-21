02812c0bdcc66bbac2b87b2776a5ad879e6e386d
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-21 17:00:45 -0300

    feat(backend): add authentication service

---
+++ backend/src/main/java/com/davisiqueira/fraud_guard/service/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.davisiqueira.fraud_guard.service.auth;
+
+import com.davisiqueira.fraud_guard.config.SecurityConfiguration;
+import com.davisiqueira.fraud_guard.dto.auth.CreateUserDTO;
+import com.davisiqueira.fraud_guard.dto.auth.LoginRequestDTO;
+import com.davisiqueira.fraud_guard.dto.auth.LoginResponseDTO;
+import com.davisiqueira.fraud_guard.model.RoleModel;
+import com.davisiqueira.fraud_guard.model.UserModel;
+import com.davisiqueira.fraud_guard.repository.UserRepository;
+import com.davisiqueira.fraud_guard.service.user.JwtService;
+import com.davisiqueira.fraud_guard.service.user.UserDetailsImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthenticationService {
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final SecurityConfiguration securityConfiguration;
+    private final UserRepository userRepository;
+
+    public AuthenticationService(AuthenticationManager authenticationManager, JwtService jwtService, SecurityConfiguration securityConfiguration, UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.securityConfiguration = securityConfiguration;
+        this.userRepository = userRepository;
+    }
+
+    public LoginResponseDTO authenticateUser(LoginRequestDTO request) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(request.email(), request.password());
+
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        return new LoginResponseDTO(jwtService.generateToken(userDetails));
+    }
+
+    public void createUser(CreateUserDTO user) {
+        UserModel newUser = UserModel.builder()
+                .email(user.email())
+                .password(securityConfiguration.passwordEncoder().encode(user.password()))
+                .roles(List.of(RoleModel.builder().name(user.role()).build()))
+                .build();
+
+        userRepository.save(newUser);
+    }
+}
