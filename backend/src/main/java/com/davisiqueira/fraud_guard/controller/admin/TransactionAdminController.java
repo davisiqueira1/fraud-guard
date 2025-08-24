1722b73d8c7323cb10d41cbde8eb5d172685a53b
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-24 16:34:37 -0300

    refactor(transaction); split transaction controller into admin and user controllers

---
+++ backend/src/main/java/com/davisiqueira/fraud_guard/controller/admin/TransactionAdminController.java
@@ -0,0 +1,51 @@
+package com.davisiqueira.fraud_guard.controller.admin;
+
+import com.davisiqueira.fraud_guard.dto.transaction.TransactionResponseDTO;
+import com.davisiqueira.fraud_guard.dto.transaction.TransactionsStatisticsDTO;
+import com.davisiqueira.fraud_guard.service.TransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@Validated
+@RequestMapping("/api/users/{userId}/transactions")
+public class TransactionAdminController {
+    private final TransactionService service;
+
+    public TransactionAdminController(TransactionService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByUser(@PathVariable Long userId) {
+        List<TransactionResponseDTO> transactions = service.getTransactionsByUserId(userId);
+
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponseDTO> getTransactionByID(@PathVariable Long userId, @PathVariable Long id) throws Exception {
+        TransactionResponseDTO transaction = service.getTransactionById(id);
+
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    @GetMapping("/suspect")
+    public ResponseEntity<List<TransactionResponseDTO>> getSuspectTransactions(@PathVariable Long userId) {
+        List<TransactionResponseDTO> suspectTransactions = service.getSuspectTransactions();
+
+        return new ResponseEntity<>(suspectTransactions, HttpStatus.OK);
+    }
+
+    @GetMapping("/statistics")
+    public ResponseEntity<TransactionsStatisticsDTO> getTransactionsStats(@PathVariable Long userId) {
+        TransactionsStatisticsDTO stats = service.getTransactionsStats();
+
+        return new ResponseEntity<>(stats, HttpStatus.OK);
+    }
+}
--- backend/src/main/java/com/davisiqueira/fraud_guard/controller/user/TransactionUserController.java
+++ backend/src/main/java/com/davisiqueira/fraud_guard/controller/user/TransactionUserController.java
@@ -1,11 +1,10 @@
-package com.davisiqueira.fraud_guard.controller;
+package com.davisiqueira.fraud_guard.controller.user;
 
 import com.davisiqueira.fraud_guard.dto.transaction.TransactionRequestDTO;
 import com.davisiqueira.fraud_guard.dto.transaction.TransactionResponseDTO;
 import com.davisiqueira.fraud_guard.dto.transaction.TransactionsStatisticsDTO;
 import com.davisiqueira.fraud_guard.security.AuthenticatedUser;
 import com.davisiqueira.fraud_guard.service.TransactionService;
-import com.davisiqueira.fraud_guard.validation.validator.cpf.ValidCpf;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +17,11 @@ import java.util.List;
 @Controller
 @Validated
 @RequestMapping("/api/transactions")
-public class TransactionController {
+public class TransactionUserController {
     private final AuthenticatedUser authenticatedUser;
     private final TransactionService service;
 
-    public TransactionController(TransactionService service, AuthenticatedUser authenticatedUser) {
+    public TransactionUserController(TransactionService service, AuthenticatedUser authenticatedUser) {
         this.service = service;
         this.authenticatedUser = authenticatedUser;
     }
@@ -38,20 +37,6 @@ public class TransactionController {
         List<TransactionResponseDTO> transactions = service.getTransactionsByUserId(authenticatedUser.get().getUser().getId());
 
         return new ResponseEntity<>(transactions, HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<TransactionResponseDTO> getTransactionByID(@PathVariable Long id) throws Exception {
-        TransactionResponseDTO transaction = service.getTransactionById(id);
-
-        return new ResponseEntity<>(transaction, HttpStatus.OK);
-    }
-
-    @GetMapping("/suspect")
-    public ResponseEntity<List<TransactionResponseDTO>> getSuspectTransactions() {
-        List<TransactionResponseDTO> suspectTransactions = service.getSuspectTransactions();
-
-        return new ResponseEntity<>(suspectTransactions, HttpStatus.OK);
     }
 
     @GetMapping("/statistics")
