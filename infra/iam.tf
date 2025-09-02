d25a7cdec3209c67a97e4eee2e74b28121c70b4f
Davi Siqueira <davisvsiqueira@gmail.com>
2025-09-02 20:22:55 -0300

    feat(lambda): add initial lambda execution role

---
+++ infra/iam.tf
@@ -0,0 +1,18 @@
+# Trust policy: decides who can assume a role
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
