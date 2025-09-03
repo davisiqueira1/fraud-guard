75c4c7a0b09dff34b597e9ed41a6e0757b93354d
Davi Siqueira <davisvsiqueira@gmail.com>
2025-09-03 19:56:22 -0300

    feat(lambda): creates mapping between SQS and Lambda

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -4,6 +4,18 @@ data "archive_file" "lambda_handler" {
   output_path = "${path.module}/build/lambda_handler.zip"
 }
 
+resource "aws_lambda_event_source_mapping" "lambda_sqs_trigger" {
+  function_name           = aws_lambda_function.lambda.function_name
+  event_source_arn        = aws_sqs_queue.sqs_queue.arn
+  function_response_types = ["ReportBatchItemFailures"]
+
+  batch_size = var.lambda_batch_size
+
+  scaling_config {
+    maximum_concurrency = 100
+  }
+}
+
 resource "aws_lambda_function" "lambda" {
   function_name = var.lambda_name
   role          = aws_iam_role.lambda_role.arn
--- infra/variables.tf
+++ infra/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 
 variable "lambda_timeout_seconds" {
-  description = "Defines the maximum time that a lambda will be executed."
+  description = "Defines the maximum time a lambda function can spend processing one message."
   type        = number
   default     = 3
 }
@@ -12,4 +12,10 @@ variable "lambda_timeout_seconds" {
 variable "lambda_name" {
   description = "Defines the lambda function name."
   type        = string
+}
+
+variable "lambda_batch_size" {
+  description = "Defines the lambda batch size when polling from SQS queue."
+  type        = number
+  default     = 10
 }
