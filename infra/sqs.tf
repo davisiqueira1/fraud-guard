e0e19b493024adc2f96d469458e384bc3d434be4
Davi Siqueira <davisvsiqueira@gmail.com>
2025-09-03 19:57:15 -0300

    refactor(sqs): multiply lambda timeout by lambda batch size

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -1,4 +1,4 @@
 resource "aws_sqs_queue" "sqs_queue" {
   name                       = "fraud-guard-sqs"
-  visibility_timeout_seconds = ceil(var.lambda_timeout_seconds * 1.25)
+  visibility_timeout_seconds = ceil((var.lambda_timeout_seconds * var.lambda_batch_size) * 1.25)
 }
