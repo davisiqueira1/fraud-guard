73ba4e34a8bbcd29ea612cf954f4232c15bdc3b0
Davi Siqueira <davisvsiqueira@gmail.com>
2025-09-06 16:06:28 -0300

    feat(api): ignore empty fields in ApiResponse serialization

--- backend/src/main/java/com/davisiqueira/fraud_guard/common/response/ApiResponse.java
+++ backend/src/main/java/com/davisiqueira/fraud_guard/common/response/ApiResponse.java
@@ -1,5 +1,8 @@
 package com.davisiqueira.fraud_guard.common.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record ApiResponse<T>(T data, PageInfo pageable) {
     public static <T> ApiResponse<T> of(T data, PageInfo pageable) {
         return new ApiResponse<>(data, pageable);
